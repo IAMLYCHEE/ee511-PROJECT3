ff5524b31dc79603a8d38d4639aafe61cd3f9891
Li Yicheng <l.y.c.liyicheng@gmail.com>
2017-02-04 12:46:47 -0800

    Delete Nhistogram.m

--- Nhistogram.m
+++
@@ -1,9 +0,0 @@
-function E = Nhistogram(eval_budget,thre)
-%use: E = Nhistogram(eval_budget,thre)
-% plot the histogram using eval_budget for N, threshold thre
-result = zeros(eval_budget,1);
-for i = 1 : eval_budget
-    result(i)= generateN(thre);
-end
-histogram(result);
-E = mean(result);
